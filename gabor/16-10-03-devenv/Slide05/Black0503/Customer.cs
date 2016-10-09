b065ffddc7500156b598e01a09995ccda6d60d22
Gabor Pinter <gaboratorium@gmail.com>
2016-10-09 13:31:05 +0200

    Day 26: Devenv Slide 5 - Object Oriented Programming

---
+++ gabor/16-10-03-devenv/Slide05/Black0503/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Black0503
+{
+    class Customer
+    {
+        public string name;
+        public List<Item> cart = new List<Item>();
+
+        // Add something to customer's cart
+        public void Buy(Item item)
+        {
+            Console.WriteLine($"{this.name} buys {item.name}  for {item.price}");
+            this.cart.Add(item);
+        }
+
+        // Output the content of the customer's cart
+        public void Checkout()
+        {
+            Console.WriteLine($"{ this.name}'s cart contains: ");
+            for (int i = 0; i < this.cart.Count; i++)
+            {
+                Console.WriteLine($"- {this.cart[i].name}, price: {this.cart[i].price}");
+            }
+        }
+    }
+}
---
+++ gabor/16-10-03-devenv/Slide05/Black0506/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Black0504
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal length = 5;
+            decimal width = 6;
+            decimal height = 7;
+            Box myBox = new Box(length, width, height);
+
+            Console.WriteLine($"The volume of the box is: {myBox.Volume}");
+        }
+    }
+}
