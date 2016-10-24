2f74a258c270afa5bc9fe3b73139b3b071a1b871
Gabor Pinter <gaboratorium@gmail.com>
2016-10-24 14:50:35 +0200

    Day 34: Devenv - Slide 07 & slide 08 - ASP.NET MVC Basic

---
+++ gabor/16-10-21-devenv/Slide07/Black0705/PDF.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Black0705
+{
+    class PDF : Document
+    {
+        public PDF(int size, string type)
+        {
+            this.Size = size;
+            this.Type = type;
+        }
+    }
+}
