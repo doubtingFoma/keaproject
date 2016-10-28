ee42136c540b7299dff737fc6cbd2cc9c5491075
Gabor Pinter <gaboratorium@gmail.com>
2016-10-28 19:02:29 +0200

    Day 37: Devenv - c# MVC

---
+++ gabor/16-10-28-devenv/Slide08/Slide08/Controllers/PostsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Slide08.Controllers
+{
+    public class PostsController : Controller
+    {
+        public List<string> Posts { get; set; }
+
+        public PostsController()
+        {
+            this.Posts = new List<string>();
+            this.Posts.Add("posts 1");
+            this.Posts.Add("posts 2");
+            this.Posts.Add("posts 3");
+        }
+
+        public ActionResult Index()
+        {
+            return Content("Hello from PostsController Index()");
+        }
+
+        // GET: Posts
+        // Test it on -http://localhost:52783/posts/details/1
+        public ActionResult Details(int id)
+        {
+            return Content(this.Posts[id-1]);
+        }
+
+    }
+}
---
+++ gabor/16-10-28-devenv/Slide08/Slide09/Controllers/PostsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Slide09.Controllers
+{
+    public class PostsController : Controller
+    {
+        // GET: Posts
+        public ActionResult Index(int id)
+        {
+            List<string> myPosts = new List<string>();
+            myPosts.Add("myVeryPost1");
+            myPosts.Add("myQuitePost2");
+            myPosts.Add("myMuchPost3");
+
+            ViewBag.myPosts = myPosts;
+            return View("Index");
+        }
+
+        public ActionResult Details(int index)
+        {
+            List<string> myPosts = new List<string>();
+            myPosts.Add("myVeryPost1");
+            myPosts.Add("myQuitePost2");
+            myPosts.Add("myMuchPost3");
+
+            ViewBag.myPost = myPosts[index];
+            return View("Details");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
